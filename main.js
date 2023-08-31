4ae428ebde6b93136b46abce38ab3fcbb640c85a
tritong <tongtri31@gmail.com>
2023-08-31 13:50:23 -0700

    Set up files for html, css, and js
    Created the calculator frame and began to fill in the container with
    divs. Calculator itself will be split into 4 parts: screen, section 1,
    section 2, and section 3.

---
+++ main.js
@@ -0,0 +1,20 @@
+function addNum(a,b){
+    return a + b;
+}
+
+function subtractNum(a,b){
+    return a - b;
+}
+
+function multiplyNum(a,b){
+    return a * b;
+}
+
+function divideNum(a,b){
+    return a/b;
+}
+
+console.log(addNum(2,7));
+console.log(subtractNum(10,7));
+console.log(multiplyNum(2,7));
+console.log(divideNum(24,3));
