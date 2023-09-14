4f3e03b1117482f605a61bd552d10622e6f99ac6
tritong <tongtri31@gmail.com>
2023-09-14 15:02:32 -0700

    Enhance button decorations and added A/C function
    Implemented visual dynamics to buttons so they light up when clicked. Set
    a limit on the numbers on display to only 10 digits and made a clear button
    to quickly reset the display screen. Having trouble with the operators ignored
    figuring out how to store two numbers to use, will look into it next time.

--- main.js
+++ main.js
@@ -37,6 +37,12 @@ console.log(divideNum(24,3));
 let streamLine = "";
 
 const display = document.querySelector('.display');
+const clearAll = document.querySelector('.button1');
+
+// Operators
+const addB = document.querySelector('.button16');
+
+// Numbers
 const num7 = document.querySelector('.button5');
 const num8 = document.querySelector('.button6');
 const num9 = document.querySelector('.button7');
@@ -48,57 +54,105 @@ const num2 = document.querySelector('.button14');
 const num3 = document.querySelector('.button15');
 const num0 = document.querySelector('.button17');
 
-num7.addEventListener('click', () => {
-    streamLine += "7";
-    display.textContent = streamLine;
-})
+// Event Listeners for the Buttons
 
-num8.addEventListener('click', () => {
-    streamLine += "8";
-    display.textContent = streamLine;
+/* !!!TODO!!! Still Need to Implement
+addB.addEventListener('click', () => {
+    const firstNum = streamLine;
+    streamLine = "";
+    const active = True;
+    while(active){
+        const secondNum = streamLine;
+        
+    }
 })
-
-num9.addEventListener('click', () => {
-    streamLine += "9";
-    display.textContent = streamLine;
-})
-
-num4.addEventListener('click', () => {
-    streamLine += "4";
-    display.textContent = streamLine;
-})
-
-num5.addEventListener('click', () => {
-    streamLine += "5";
-    display.textContent = streamLine;
-})
-
-num6.addEventListener('click', () => {
-    streamLine += "6";
-    display.textContent = streamLine;
-})
-
-num1.addEventListener('click', () => {
-    streamLine += "1";
-    display.textContent = streamLine;
-})
-
-num2.addEventListener('click', () => {
-    streamLine += "2";
-    display.textContent = streamLine;
-})
-
-num3.addEventListener('click', () => {
-    streamLine += "3";
-    display.textContent = streamLine;
+*/
+clearAll.addEventListener('click', () => {
+    streamLine = "";
+    display.textContent = 0;
 })
 
 num0.addEventListener('click', () => {
     if(display.textContent == '0'){
         display.textContent = display.textContent;
     }
+    else if(streamLine.length >= 10){
+        return;
+    }
     else{
         streamLine += "0";
         display.textContent = streamLine;
     }
+})
+
+num1.addEventListener('click', () => {
+    if(streamLine.length >= 10){
+        return;
+    }
+    streamLine += "1";
+    display.textContent = streamLine;
+})
+
+num2.addEventListener('click', () => {
+    if(streamLine.length >= 10){
+        return;
+    }
+    streamLine += "2";
+    display.textContent = streamLine;
+})
+
+num3.addEventListener('click', () => {
+    if(streamLine.length >= 10){
+        return;
+    }
+    streamLine += "3";
+    display.textContent = streamLine;
+})
+
+num4.addEventListener('click', () => {
+    if(streamLine.length >= 10){
+        return;
+    }
+    streamLine += "4";
+    display.textContent = streamLine;
+})
+
+num5.addEventListener('click', () => {
+    if(streamLine.length >= 10){
+        return;
+    }
+    streamLine += "5";
+    display.textContent = streamLine;
+})
+
+num6.addEventListener('click', () => {
+    if(streamLine.length >= 10){
+        return;
+    }
+    streamLine += "6";
+    display.textContent = streamLine;
+})
+
+num7.addEventListener('click', () => {
+    if(streamLine.length >= 10){
+        return;
+    }
+    streamLine += "7";
+    display.textContent = streamLine;
+})
+
+num8.addEventListener('click', () => {
+    if(streamLine.length >= 10){
+        return;
+    }
+    streamLine += "8";
+    display.textContent = streamLine;
+})
+
+num9.addEventListener('click', () => {
+    if(streamLine.length >= 10){
+        return;
+    }
+    streamLine += "9";
+    display.textContent = streamLine;
 })
